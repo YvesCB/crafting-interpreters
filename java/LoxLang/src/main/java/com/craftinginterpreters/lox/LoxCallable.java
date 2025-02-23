b92aad40e0f8a9f903478d56af709c92daf21d09
YvesCB <y.bruckmayer@gmail.com>
2025-02-23 14:11:22 +0100

    finished ch 11 + 12

--- java/LoxLang/src/main/java/com/craftinginterpreters/lox/LoxCallable.java
+++ java/LoxLang/src/main/java/com/craftinginterpreters/lox/LoxCallable.java
@@ -3,7 +3,7 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 interface LoxCallable {
-    int arity();
+  int arity();
 
-    Object call(Interpreter interpreter, List<Object> arguments);
+  Object call(Interpreter interpreter, List<Object> arguments);
 }
--- java/LoxLang/src/main/java/com/craftinginterpreters/lox/Return.java
+++ java/LoxLang/src/main/java/com/craftinginterpreters/lox/Return.java
@@ -1,10 +1,10 @@
 package com.craftinginterpreters.lox;
 
 public class Return extends RuntimeException {
-    final Object value;
+  final Object value;
 
-    Return(Object value) {
-        super(null, null, false, false);
-        this.value = value;
-    }
+  Return(Object value) {
+    super(null, null, false, false);
+    this.value = value;
+  }
 }
