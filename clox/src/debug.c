bd99f92408ea3582ad65b285bdec269705d68eab
YvesCB <y.bruckmayer@gmail.com>
2025-02-27 21:20:15 +0100

    implementing the vm

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -36,10 +36,12 @@ int dissassembleInstruction(Chunk *chunk, int offset) {
   uint8_t instruction = chunk->code[offset];
 
   switch (instruction) {
-  case OP_RETURN:
-    return simpleInstruction("OP_RETURN", offset);
   case OP_CONSTANT:
     return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
+  case OP_RETURN:
+    return simpleInstruction("OP_RETURN", offset);
   default:
     printf("Unknown instruction %d\n", instruction);
     return offset + 1;
