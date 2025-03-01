1351e83bb4bf98aa7293cf57588f9ca73d0369a4
YvesCB <y.bruckmayer@gmail.com>
2025-03-01 12:28:58 +0100

    working on parsing

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -14,6 +14,10 @@ void initScanner(const char *source) {
 
 static bool isDigit(char c) { return c >= '0' && c <= '9'; }
 
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
 static bool isAtEnd() { return *scanner.current == '\0'; }
 
 static Token makeToken(TokenType type) {
@@ -85,7 +89,66 @@ static void skipWhitespace() {
   }
 }
 
-static Token number() {}
+static TokenType checkKeyword(int start, int length, const char *rest,
+                              TokenType type) {
+  if (scanner.current - scanner.start == start + length &&
+      memcmp(scanner.start + start, rest, length) == 0) {
+    return type;
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+  switch (scanner.start[0]) {
+  case 'a':
+    return checkKeyword(1, 2, "nd", TOKEN_AND);
+  case 'c':
+    return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+  case 'e':
+    return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+  case 'i':
+    return checkKeyword(1, 1, "f", TOKEN_IF);
+  case 'n':
+    return checkKeyword(1, 2, "il", TOKEN_NIL);
+  case 'o':
+    return checkKeyword(1, 1, "r", TOKEN_OR);
+  case 'p':
+    return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+  case 'r':
+    return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+  case 's':
+    return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+  case 'v':
+    return checkKeyword(1, 2, "ar", TOKEN_VAR);
+  case 'w':
+    return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+  }
+
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek()))
+    advance();
+  return makeToken(identifierType());
+}
+
+static Token number() {
+  while (isDigit(peek()))
+    advance();
+
+  // Look for fractional part
+  if (peek() == '.' && isDigit(peekNext())) {
+    // consume .
+    advance();
+
+    while (isDigit(peek()))
+      advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
 
 static Token string() {
   while (peek() != '"' && !isAtEnd()) {
@@ -110,6 +173,8 @@ Token scanToken() {
 
   char c = advance();
 
+  if (isAlpha(c))
+    return identifier();
   if (isDigit(c))
     return number();
 
