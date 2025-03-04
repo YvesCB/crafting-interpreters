23417358e9baf3bf5cff54b36278ac7224e2bf05
YvesCB <y.bruckmayer@gmail.com>
2025-03-04 20:39:40 +0100

    finished ch 17

--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -4,6 +4,12 @@
 #include "common.h"
 #include "scanner.h"
 
+typedef struct {
+  const char *start;
+  const char *current;
+  int line;
+} Scanner;
+
 Scanner scanner;
 
 void initScanner(const char *source) {
