39172e489dbd90f2ae67cd9273129d2587951cff
YvesCB <y.bruckmayer@gmail.com>
2025-09-07 17:27:14 +0200

    up to 25.2.2
    Also changed clang-format so many line changes

--- clox/src/memory.h
+++ clox/src/memory.h
@@ -3,21 +3,20 @@
 
 #include "common.h"
 
-#define ALLOCATE(type, count)                                                  \
-  (type *)reallocate(NULL, 0, sizeof(type) * (count))
+#define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount)                          \
-  (type *)reallocate(pointer, sizeof(type) * (oldCount),                       \
-                     sizeof(type) * (newCount))
+    (type*)reallocate(pointer, sizeof(type) * (oldCount),                      \
+                      sizeof(type) * (newCount))
 
 #define FREE_ARRAY(type, pointer, oldCount)                                    \
-  (type *)reallocate(pointer, sizeof(type) * (oldCount), 0)
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 #define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
-void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 void freeObjects();
 
 #endif // !clox_memory_h
